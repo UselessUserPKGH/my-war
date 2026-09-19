8ffde634104b1fd9d47de32e7bc0ee720c3cec80
Application_Belchenko <testylya333@gmail.com>
2026-09-19 14:22:05 +0300

    init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=belchenko_my-war',
+    'username' => 'belchenko_my-war',
+    'password' => 'Admin12345*',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
